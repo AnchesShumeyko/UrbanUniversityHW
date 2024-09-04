1b11d6915ac8e0c98a818f8dac6ecfbacd67b561
Anches <ourson13@mail.ru>
2024-09-04 08:19:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# 1st problem
+print(9 ** 0.5 * 5)
+# 2nd problem
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3d problem
+print(2 * 2 + 2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+#4th problem
+print(int(float ('123.456') * 10) % 10)
