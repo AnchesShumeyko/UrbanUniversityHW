92e57707a4d576c205e29d075ddede5d9c86c7b2
Anches <ourson13@mail.ru>
2024-09-05 21:28:38 +0300

    Update modul_1_2.py

--- modul_1_2.py
+++ modul_1_2.py
@@ -1,8 +1,7 @@
 HW_amount = 12
 hours_of_working = 1.5
 course_name = 'Python'
-print('Время на одно задание:',  hours_of_working / HW_amount, 'часа')
-print('Время на одно задание:',  hours_of_working / HW_amount * 60, 'минут')
-# время в более удобоваримом формате :-)
-print('Время на одно задание:',  hours_of_working / HW_amount, 'часа (', hours_of_working / HW_amount * 60, 'минут)')
-
+av_time = hours_of_working / HW_amount
+print('Курс:', course_name, ', всего выполнено задач: ',
+      HW_amount, ', затрачено часов: ', hours_of_working,
+      ', среднее время выполнения: ', av_time, 'часа (', av_time *60, 'минут)')
