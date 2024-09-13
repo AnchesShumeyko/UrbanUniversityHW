cb489b3b08465906e694b6c7b1e5b824ff7c1776
Anches <ourson13@mail.ru>
2024-09-13 22:11:07 +0300

    Add files via upload

---
+++ modul_3_1.py
@@ -0,0 +1,37 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+def string_info(string):
+    global calls
+    calls += 1
+    a = string.lower()
+    b = string.upper()
+    c = len(string)
+    return c, a, b
+
+def is_contains(string, list_ = []):
+    global calls
+    calls += 1
+    a = string.lower()
+    for i in list_:
+        b = i.lower()
+        if a == b:
+            return True
+    if a != b:
+        return False
+
+    return a, b
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+print(count_calls())
+
+
+
