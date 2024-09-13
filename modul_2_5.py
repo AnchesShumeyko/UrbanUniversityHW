259fd58e0943a7193151c83a705a717acb580409
Anches <ourson13@mail.ru>
2024-09-13 11:00:13 +0300

    Update modul_2_5.py

--- modul_2_5.py
+++ modul_2_5.py
@@ -1,13 +1,13 @@
-n = int(input('введите количество строк: '))
-m = int(input('введите количество столбцов: '))
-value = int(input('введите значение: '))
-matrix = []
-def get_matrix(n, m, value):
+def get_matrix(n, m = 0, value = 0):
+    matrix = []
     for i in range(1, n + 1):
         list1 = []
         matrix.append(list1)
         for j in range(1, m + 1):
             list1.append(value)
-get_matrix(n, m, value)
-print(matrix)
+    print(matrix)
 
+get_matrix(2,2,10)
+get_matrix(3,5,42)
+get_matrix(4,2,13)
+get_matrix(0)
