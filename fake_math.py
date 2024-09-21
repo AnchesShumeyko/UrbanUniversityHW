83343bfb8528b71a9cbf3562a6ca7d80641300ea
Anches <ourson13@mail.ru>
2024-09-21 22:21:59 +0300

    fake_math.py

--- fake_math.py
+++ fake_math.py
@@ -1,9 +1,5 @@
-#создайте функцию divide, которая принимает два параметра first и second.
-# Функция должна возвращать результат деления first на second,
-# но когда в second записан 0 - возвращать строку 'Ошибка'.
-
 def divide(first, second):
     if second != 0:
         return first / second
-    elif second == 0:
+    else:
         return 'ошибка'
