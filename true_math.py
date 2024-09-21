a27da5713970d005eb849ee5ec785f1efb811fe1
Anches <ourson13@mail.ru>
2024-09-21 22:22:33 +0300

    true_math.py

--- true_math.py
+++ true_math.py
@@ -1,8 +1,3 @@
-# создайте функцию divide, которая принимает два параметра first и second.
-# Функция должна возвращать результат деления first на second,
-# но когда в second записан 0 - возвращать бесконечность.
-# Бесконечность можно импортировать из встроенной библиотеки
-# math (from math import inf)
 from math import inf
 
 def divide(first, second):
