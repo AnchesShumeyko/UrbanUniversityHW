43f779b0d31cc448ab647bb4e191a901046ec5d3
Anches <ourson13@mail.ru>
2024-09-22 00:52:16 +0300

    Update modul_3_5.py

--- modul_3_5.py
+++ modul_3_5.py
@@ -1,10 +1,14 @@
 def get_multiplied_digits(n):
-    str_number = str(n)
+    str_n = str(n) # переводим в строки
+    str_number = str_n.replace('0', '') # отбрасываем нули
+
     first = int(str_number[0])
+    
     if len(str_number) == 1:
         return first
-    else:
-        return first * get_multiplied_digits(int(str_number[1:]))
+    if len(str_number) > 1:
+        if first != '0':
+            return first * get_multiplied_digits(int(str_number[1:]))
 
 print(get_multiplied_digits(40203))
-print(get_multiplied_digits(7392875)) # для проверки еще раз
+print(get_multiplied_digits(420)) 
