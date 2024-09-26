b14f39593e2d5607effdbe18b0abf4b0c79564ff
Anches <ourson13@mail.ru>
2024-09-26 16:33:01 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -12,13 +12,13 @@ class House():  # создаем класс "House"
                 print('Такого этажа не существует')
                 break
 
-    def __len__(self): #метод возвращающий кол-во этажей
+    def __len__(self):  # метод возвращающий кол-во этажей
         return self.number_of_floors
 
-    def __str__(self): # метод, возвращающий имя и этажи
+    def __str__(self):  # метод, возвращающий имя и этажи
         return (f'Название: "{self.name}", кол-во этажей: {self.number_of_floors}.')
 
-#методы для сравнения количества этажей
+    # методы для сравнения количества этажей
     def __eq__(self, other):
         if not isinstance(other, int) and not isinstance(other, House):
             return print('ошибка')
@@ -49,21 +49,18 @@ class House():  # создаем класс "House"
             return print('ошибка')
         return self.number_of_floors != other.number_of_floors
 
-# методы для сложения
+    # методы для сложения
     def __add__(self, other):
         if isinstance(other, int):
-         self.number_of_floors = self.number_of_floors + other
-        if not isinstance(other, int):
-            return print('ошибка')
+            self.number_of_floors = self.number_of_floors + other
         return self
 
     def __radd__(self, other):
-        self.number_of_floors = other + self.number_of_floors
-        return self
+        return self.__add__(other)
 
     def __iadd__(self, other):
-        self.number_of_floors += other
-        return self
+        return self.__add__(other)
+
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
@@ -71,20 +68,20 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2) # __eq__
+print(h1 == h2)  # __eq__
 
-h1 = h1 + '10' # __add__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
 
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
