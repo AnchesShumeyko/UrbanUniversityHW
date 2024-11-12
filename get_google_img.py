de212a93e6f6f4ef5ee3e4dbd289655775b5bdc8
Anches <ourson13@mail.ru>
2024-11-12 19:46:11 +0300

    Update get_google_img.py

--- get_google_img.py
+++ get_google_img.py
@@ -1,45 +1,46 @@
-from encodings.utf_16 import encode
-
 from icrawler.builtin import GoogleImageCrawler
 import os
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 from random import randint
 import requests
 import datetime
 
 
 """Функция для получения картинок города из Google"""
-# def get_city_picture(city):
+def get_city_picture(city):
+
+    if not os.path.exists('city'):
+        os.mkdir('city')
+    request_word = city
+    google = GoogleImageCrawler(storage={'root_dir' : '/Users/anchesshumeyko/PycharmProjects/iMac_Anches/module_11/city'})
+    google.crawl(keyword=request_word, max_num=10)
 #
-#     if not os.path.exists('city'):
-#         os.mkdir('city')
-#     request_word = city
-#     google = GoogleImageCrawler(storage={'root_dir' : '/Users/anchesshumeyko/PycharmProjects/iMac_Anches/module_11/city'})
-#     google.crawl(keyword=request_word, max_num=10)
-#
-# get_city_picture('Moscow')
+
+"""Чистим директорию"""
+def clear_directory():
+    os.chdir('/Users/anchesshumeyko/PycharmProjects/iMac_Anches/module_11/city')
+    for f in os.listdir():
+        if os.path.isfile(f):
+            os.remove(f)
 
 """Функция рандомно выбирает картинку из скаченных 10"""
-# def choose_pic():
-#     os.chdir('city')
-#     filename = f'{str(randint(1,10)).zfill(6)}.jpg'
-#     image = Image.open(filename)
-#     image.show()
-#     return filename
+def choose_pic():
+    filename = f'{str(randint(1,10)).zfill(6)}.jpg'
+    return filename
 #
-# choose_pic()
-
+# # choose_pic()
+#
 """Функция изменяет размер картинки под необходимый размер для постера"""
-def change_size(filename):
-    os.chdir('city')
+def change_size(picture):
+    os.chdir('/Users/anchesshumeyko/PycharmProjects/iMac_Anches/module_11/city')
     fixed_width = 500
-    poster_image = Image.open(filename)
+    poster_image = Image.open(picture)
     width_proportion = (fixed_width / float(poster_image.size[0]))
     height_size = int((poster_image.size[0]) * float(width_proportion))
     resized_image = poster_image.resize((fixed_width, height_size))
-    return resized_image
-#
-# change_size('000010.jpg')
+    resized_image.save('poster_picture.jpg')
+    return 'poster_picture.jpg'
+
 
 """Получение погоды необходимого города"""
 def get_weather(city):
@@ -52,31 +53,30 @@ def get_weather(city):
         pressure = json_object['main']['pressure']
         humidity = json_object['main']['humidity']
         wind = json_object['wind']['speed']
-        return (f'{city}\nтемпература {round(temp)} C\nвлажность {humidity}%\nатмосферное давление {pressure} мм/рт ст'
-                f'\nскорость ветра {wind} м/с')
+        return (f'{city.capitalize()}\n\ntemperature {round(temp)} C\nhumidity {humidity}%\npressure {pressure} mmHg'
+                f'\nwind speed {wind} m/s')
     else:
         return f'невозможно получить ответ с сайта, просто выгляните в окно'
 
 """Формируем постер для города"""
-# def make_poster()
-date = datetime.date.today()
-main_canvas = Image.new('RGB', (500, 1200), 'lightblue')
-# poster = ImageDraw.Draw(main_canvas)
-city_pic = change_size('000010.jpg')
-#
-drawer = ImageDraw.Draw(main_canvas)
-text = get_weather('Tver')
-drawer.text((10, 600), text=text, encode='utf-8', fill='black', font_size=30)
-main_canvas.paste(city_pic, (0,0))
-main_canvas.show()
+def make_poster(city_pic, text, city):
+    date = datetime.date.today()
+    main_canvas = Image.new('RGB', (500, 900), 'lightblue')
+    drawer = ImageDraw.Draw(main_canvas)
+    drawer.text((10, 600), text=text, fill='black', font_size=30)
+    image1 = Image.open('/Users/anchesshumeyko/PycharmProjects/iMac_Anches/module_11/city/poster_picture.jpg')
+    main_canvas.paste(image1,(0,0))
+    main_canvas.save(f'/Users/anchesshumeyko/PycharmProjects/iMac_Anches/module_11/{city}-{date}.jpg')
+    main_canvas.show()
 
+def main(city):
+    get_city_picture(city)
+    picture = choose_pic()
+    resized_picture = change_size(picture)
+    weather = get_weather(city)
+    make_poster(resized_picture, weather, city)
+    clear_directory()
 
-
-
-
-
-
-
-
+main('Moscow')
 
 
