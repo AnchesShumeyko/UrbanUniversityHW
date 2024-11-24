0ec6edf2e0bc2c0ed02efa1432e73e836033e3e7
Anches <ourson13@mail.ru>
2024-11-24 21:17:35 +0300

    Add files via upload

--- module_12_2.py
+++ module_12_2.py
@@ -1,33 +1,51 @@
+import tournament
 import unittest
-import runner # импортирован модуль с классом Runner
 
 
-class RunnerTest(unittest.TestCase):
-    def test_walk(self):
-        runner_ = runner.Runner('Penguin')
-        for _ in range(10):
-            runner_.walk()
-        self.assertEqual(runner_.distance, 45) # изменено значение равенства, данный тест не пройден
+class TournamentTest(unittest.TestCase):
+    all_results = {}
 
-    def test_run(self):
-        runner_ = runner.Runner('Koala')
-        for _ in range(10):
-            runner_.run()
-        self.assertEqual(runner_.distance, 100)
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
 
-    def test_challenge(self):
-        runner1 = runner.Runner('Turtle')
-        runner2 = runner.Runner('Snail')
-        for _ in range(10):
-            runner1.run()
-            runner2.walk()
-        self.assertNotEqual(runner1.distance, runner2.distance)
+    def setUp(self):
+        self.runner1 = tournament.Runner('Усейн', 10)
+        self.runner2 = tournament.Runner('Андрей', 9)
+        self.runner3 = tournament.Runner('Ник', 3)
 
-if __name__ == '__main__':
+    @classmethod
+    def tearDownClass(cls):  # "????"
+        for tournament_name, placements in cls.all_results.items():
+            result_str = f'{tournament_name} тур. ' + ', '.join(
+                f'{place}: {runner.name}' for place, runner in placements.items())
+            print(result_str)
 
+    def test_tournament_1(self):
+        tour1 = tournament.Tournament(90, self.runner1, self.runner3)
+        results = tour1.start()
+        TournamentTest.all_results[1] = results
+        self.assertEqual(list(results.values())[-1].name, 'Ник')
+        self.assertTrue(results[max(results.keys())] == self.runner3)
+
+    def test_tournament_2(self):
+        tour2 = tournament.Tournament(90, self.runner2, self.runner3)
+        results = tour2.start()
+        TournamentTest.all_results[2] = results
+        self.assertEqual(list(results.values())[-1].name, 'Ник')
+        self.assertTrue(results[max(results.keys())] == self.runner3)
+
+    def test_tournament_3(self):
+        tour3 = tournament.Tournament(90, self.runner1, self.runner2, self.runner3)
+        results = tour3.start()
+        TournamentTest.all_results[3] = results
+        self.assertEqual(list(results.values())[-1].name, 'Ник')
+        self.assertTrue(results[max(results.keys())] == self.runner3)
+
+
+if __name__ == "__main__":
     unittest.main()
 
-
-
-
-
+# Чтобы исправить возможную логическую ошибку, вызванную тем, что участники с меньшей
+# скоростью могут финишировать первыми, Вы можете внести изменения в метод start, чтобы можно было
+# учитывать скорость участников.
